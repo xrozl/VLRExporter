3fa3649554792198d1e28e24360ba5aab4b76512
xrozl <s1F102200064@toyo.jp>
2022-05-15 02:04:17 +0900

    vlrbase

--- src/main/java/com/github/xrozl/vlr/VLRExporter.java
+++ src/main/java/com/github/xrozl/vlr/VLRExporter.java
@@ -1,8 +1,10 @@
 package com.github.xrozl.vlr;
 
+import com.github.xrozl.vlr.api.VLRAPI;
+
 public class VLRExporter {
 
     public static void main(String[] args) {
-
+        VLRAPI.getResults(1);
     }
 }
--- src/main/java/com/github/xrozl/vlr/api/VLRAPI.java
+++ src/main/java/com/github/xrozl/vlr/api/VLRAPI.java
@@ -1,5 +1,10 @@
 package com.github.xrozl.vlr.api;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +14,26 @@ public class VLRAPI {
 
     public static List<ValorantMatch> getResults(int page) {
         List<ValorantMatch> matches = new ArrayList<>();
-
+        try {
+            String html = Jsoup.connect(URL_RESULTS).get().html();
+            //System.out.println(html);
+            Document doc = Jsoup.parse(html);
+            int c = 0;
+            for (Element element : doc.getElementsByClass("match-item-event text-of")) {
+                String n = element.toString().split("</div>")[1].replaceFirst(" ", "").replaceAll("\n", "");
+                ValorantMatch mt = new ValorantMatch();
+                mt.setName_tournament(n);
+                matches.add(mt);
+            }
+            for (Element element : doc.getElementsByClass("match-item-event-series text-of")) {
+                matches.get(c).setName_series(element.text());
+                c++;
+            }
+            c = 0;
+            for (Element element : doc.getElementsByClass())
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return matches;
     }
 }
